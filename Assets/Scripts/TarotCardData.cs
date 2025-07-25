753ddd99c4c3512e7c41c16a43d71eaa9b3f496a
MajidHussain10775 <majidhussainkhan406@gmail.com>
2025-07-25 20:00:45 +0500

    ScriptableObjectsFixedWithPRoperNameOnly

--- Assets/Scripts/TarotCardData.cs
+++ Assets/Scripts/TarotCardData.cs
@@ -50,7 +50,7 @@ public enum TarotCardType
     BetrayedCouple, // NEW: Removes all heart cards from both player and dealer hands
     Blacksmith,  // NEW: Removes all spade cards from both player and dealer hands
     TaxCollector, // NEW: Removes all diamond cards from both player and dealer hands
-    HouseKeeper,, // NEW: Adds +10 bonus per Jack/Queen/King in player's winning hand
+    HouseKeeper, // NEW: Adds +10 bonus per Jack/Queen/King in player's winning hand
     
     // NEW PREVIEW CARDS - Allow peeking at future cards
     Spy,         // NEW: Allows to peek at the next enemy card (dealer's next card)
@@ -58,7 +58,7 @@ public enum TarotCardType
     CorruptJudge, // NEW: Peek into the next three cards in your hand and rearrange the first two if desired
     Hitman,      // NEW: Peek into the first three cards on your deck and remove one at discretion from play
     FortuneTeller, // NEW: Take a peek into the next two cards on your deck
-    MadWriter    // NEW: Take a look at the next card and shuffle the whole deck if desired
+    MadWriter,    // NEW: Take a look at the next card and shuffle the whole deck if desired
     WhisperOfThePast, //Lose ¼ of your bet, temporarily discard all cards from your played side
     Saboteur,     //Lose ¼ of your bet, temporarily discard all cards from the enemy's played side
     Scammer      //Reverts the enemy's winning play discarding the played card at the cost of half the bet
